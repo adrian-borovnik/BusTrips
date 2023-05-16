d35b1d0f16502a9f2856a244391b8f8ae48681f7
adrian-borovnik <adrian.borovnik@gmail.com>
2023-05-16 18:07:01 +0200

    Printing out times in relative format and some error checking

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class Main {
@@ -21,19 +22,41 @@ public class Main {
 
         String stopID = args[0];
         int numOfBuses = Integer.parseInt(args[1]);
+
+        if(numOfBuses < 1) {
+            System.out.println("Choose a positive number of buses displayed.");
+            return;
+        }
+
         String mode = args[2];
 
-        LocalTime currentTime = LocalTime.now();
-//        String currentTimeString = currentTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-        System.out.println(currentTime);
+        if(!(mode.equals("absolute") || mode.equals("relative"))){
+            System.out.println("Invalid mode specified. Choose between \"absolute\" and \"relative\"");
+            return;
+        }
+
+        CSV stopTimes = new CSV("gtfs/stop_times.txt", new String[]{"trip_id", "stop_id", "arrival_time"});
+        ArrayList<HashMap<String, String>> timeTable = stopTimes.getTable();
+
+        boolean doesStopExist = false;
+        for(HashMap<String, String> row : timeTable){
+            if(row.get("stop_id").equals(stopID)){
+                doesStopExist = true;
+                break;
+            }
+        }
+
+        if(!doesStopExist){
+            System.out.println("There are no buses arriving at a stop with ID " + stopID + ".");
+            return;
+        }
 
         CSV stops = new CSV("gtfs/stops.txt", new String[]{"stop_id", "stop_name"});
-        CSV stopTimes = new CSV("gtfs/stop_times.txt", new String[]{"trip_id", "stop_id", "arrival_time"});
-        CSV trips = new CSV("gtfs/trips.txt", new String[]{"trip_id", "route_id"});
-
         ArrayList<HashMap<String, String>> stopsTable = stops.getTable();
-        ArrayList<HashMap<String, String>> timeTable = stopTimes.getTable();
+
+        CSV trips = new CSV("gtfs/trips.txt", new String[]{"trip_id", "route_id"});
         ArrayList<HashMap<String, String>> tripsTable = trips.getTable();
+
 
         timeTable.sort((o1, o2) -> {
             LocalTime time1 = LocalTime.parse(o1.get("arrival_time"));
@@ -41,6 +64,9 @@ public class Main {
 
             return time1.compareTo(time2);
         });
+
+        LocalTime currentTime = LocalTime.now();
+//        String currentTimeString = currentTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
 
         for (int i = timeTable.size() - 1; i >= 0; --i) {
 
@@ -71,12 +97,15 @@ public class Main {
 //            timeTable.get(i).remove("trip_id");
         }
 
-        System.out.println(timeTable);
-
-        // TODO print out stop name
+        for (HashMap<String, String> row : stopsTable) {
+            if (row.get("stop_id").equals(stopID)) {
+                System.out.println(row.get("stop_name"));
+                break;
+            }
+        }
 
         ArrayList<String> routes = new ArrayList<>();
-        for (int i = 0; i < timeTable.size() || routes.size() == numOfBuses; ++i) {
+        for (int i = 0; i < timeTable.size(); ++i) {
             String routeID = timeTable.get(i).get("route_id");
             if (isInArray(routes, routeID)) continue;
 
@@ -85,18 +114,23 @@ public class Main {
 
             for (HashMap<String, String> row : timeTable) {
 
-                // TODO consider relative time
+                if (row.get("route_id").equals(routeID)) {
+                    LocalTime arrivalTime = LocalTime.parse(row.get("arrival_time"));
+                    arrivalTime = LocalTime.of(arrivalTime.getHour(), arrivalTime.getMinute());
 
-                if (row.get("route_id").equals(routeID))
-                    System.out.print(row.get("arrival_time") + ", ");
+                    if (mode.equals("absolute"))
+                        System.out.print(arrivalTime + ", ");
+                    else if (mode.equals("relative"))
+                        System.out.print(currentTime.until(arrivalTime, ChronoUnit.MINUTES) + "min, ");
+
+                }
             }
 
             System.out.println();
+            if (routes.size() == numOfBuses) break;
 
         }
 
         System.out.println();
-
-        System.out.println("Hellooo!");
     }
 }
